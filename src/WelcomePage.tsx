a4bad931dad3c1510b2b8c22718fc0d382aa36d9
scx1332 <sieciech.czajka@golem.network>
2023-04-09 13:52:37 +0200

    Plotly experiments

--- src/WelcomePage.tsx
+++ src/WelcomePage.tsx
@@ -2,10 +2,33 @@ import React, { useContext } from "react";
 import "./WelcomePage.css";
 import { BackendSettingsContext } from "./BackendSettingsProvider";
 import { useBackendConfig } from "./ConfigProvider";
+import Plot from 'react-plotly.js';
+
+class PlotData {
+    x: number[] = [];
+    y: number[] = [];
+}
 
 const WelcomePage = () => {
     const { backendSettings } = useContext(BackendSettingsContext);
     const config = useBackendConfig();
+
+    const [plotData, setPlotData] = React.useState<PlotData>({x: [], y: []});
+
+    React.useEffect(() => {
+        (async () => {
+            const response = await fetch(backendSettings.backendUrl + "/time_hist.csv");
+            const response_csv = await response.text();
+            const lines = response_csv.split("\n");
+            const plotData = new PlotData();
+            for (const line_idx in lines) {
+                const tokens = lines[line_idx].split(",");
+                plotData.x.push(parseInt(line_idx));
+                plotData.y.push(parseInt(tokens[1]));
+            }
+            setPlotData(plotData);
+        })();
+    }, [setPlotData]);
 
     return (
         <div className="welcome-page">
@@ -13,6 +36,21 @@ const WelcomePage = () => {
             <p>Connected to the endpoint {backendSettings.backendUrl}</p>
             <p>Frontend version {APP_VERSION}</p>
             <p>Backend version {config.version}</p>
+
+            <Plot
+
+                data={[
+                    {
+                        x: plotData.x,
+                        y: plotData.y,
+                        type: 'scatter',
+                        mode: 'lines+markers',
+                        marker: {color: 'red'},
+                    },
+                    {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+                ]}
+                layout={ {width: 1600, height: 500, title: 'A Fancy Plot'} }
+            />
         </div>
     );
 };
